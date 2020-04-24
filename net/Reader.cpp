fffa4ead698452c48920c7b95a1168b7acd19257
Volker Christian <volker.christian@fh-hagenberg.at>
2020-04-24 10:33:46 +0200

    Lowlevel read/write (send/recv) moved to Reader/writer

---
+++ net/Reader.cpp
@@ -0,0 +1,27 @@
+#include "Reader.h"
+
+#include <iostream>
+
+#include <string.h>
+#include <sys/types.h>
+#include <sys/socket.h>
+
+#include "SocketMultiplexer.h"
+
+void Reader::readEvent() {
+    #define MAX_JUNKSIZE 4096
+    char junk[MAX_JUNKSIZE];
+    
+    ssize_t ret = recv(dynamic_cast<Descriptor*>(this)->getFd(), junk, MAX_JUNKSIZE, 0);
+    
+    if (ret > 0) {
+        readPuffer.append(junk, ret);
+    } else if (ret == 0) {
+        std::cout << "EOF: " << dynamic_cast<Descriptor*>(this)->getFd() << std::endl;
+        SocketMultiplexer::instance().getReadManager().unmanageSocket(this);
+    } else {
+        std::cout << "Read: " << strerror(errno) << std::endl;
+        SocketMultiplexer::instance().getReadManager().unmanageSocket(this);
+    }
+}
+
