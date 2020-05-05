aa47185369e39c353e82fab6ad5f2e099d819b01
Volker Christian <volker.christian@fh-hagenberg.at>
2020-05-05 12:14:59 +0200

    Simple Webserver

--- main.cpp
+++ main.cpp
@@ -10,7 +10,8 @@
 
 #include "httputils.h"
 
-int main(int argc, char **argv) {
+
+int timerApp(int argc, char** argv) {
     Timer& tick = Timer::continousTimer(
         [] (const void* arg) -> void {
             static int i = 0;
@@ -24,61 +25,105 @@ int main(int argc, char **argv) {
             std::cout << (const char*) arg << " " << i++ << std::endl;
         }, 
         (struct timeval) {1, 100000}, "Tack");
-        
+    
     bool canceled = false;
     HTTPServer& app = HTTPServer::instance();
     
     app.serverRoot("/home/voc/projects/ServerVoc/build/html");
     
     app.get("/",
-        [&] (const Request& req, const Response& res) -> void {
-            std::string uri = req.requestURI();
-            
-            if (uri == "/") {
-                uri = "/index.html";
-            }
-            
-            std::cout << "RCookie: " << req.cookie("doxygen_width") << std::endl;
-            std::cout << "RCookie: " << req.cookie("Test") << std::endl;
-            
-//            std::cout << "RHeader: " << req.header("Content-Length") << std::endl;
-            std::cout << "RHeader: " << req.header("Accept") << std::endl;
-            
-            std::cout << "If-Modified: " << req.header("If-Modified-Since") << std::endl;
-
-            std::cout << "RQuery: " << req.query("Hallo") << std::endl;
-            
-            res.cookie("Test", "me");
-            
-//            res.set("Connection", "close");
-            res.sendFile(uri, [uri] (int ret) -> void {
-                if (ret != 0) {
-                    perror(uri.c_str());
-//                    std::cout << "Error: " << ret << ", " << uri << std::endl;
+            [&] (const Request& req, const Response& res) -> void {
+                std::string uri = req.requestURI();
+                
+                if (uri == "/") {
+                    uri = "/index.html";
                 }
-            });
-            
-            if (!canceled) {
-                tack.cancel();
-                canceled = true;
-//                app.destroy();
+                
+                std::cout << "RCookie: " << req.cookie("doxygen_width") << std::endl;
+                std::cout << "RCookie: " << req.cookie("Test") << std::endl;
+                
+                //            std::cout << "RHeader: " << req.header("Content-Length") << std::endl;
+                std::cout << "RHeader: " << req.header("Accept") << std::endl;
+                
+                std::cout << "If-Modified: " << req.header("If-Modified-Since") << std::endl;
+                
+                std::cout << "RQuery: " << req.query("Hallo") << std::endl;
+                
+                res.cookie("Test", "me");
+                
+                //            res.set("Connection", "close");
+                res.sendFile(uri, [uri] (int ret) -> void {
+                    if (ret != 0) {
+                        perror(uri.c_str());
+                        //                    std::cout << "Error: " << ret << ", " << uri << std::endl;
+                    }
+                });
+                
+                if (!canceled) {
+                    tack.cancel();
+                    canceled = true;
+                    //                app.destroy();
+                }
+                //            Multiplexer::stop();
             }
-//            Multiplexer::stop();
-        }
     );
     
     app.listen(8080, 
-        [] (int err) -> void {
-            if (err != 0) {
-                perror("Listen");
-            } else {
-                std::cout << "Up and running" << std::endl;
-            }
-        }
+               [] (int err) -> void {
+                   if (err != 0) {
+                       perror("Listen");
+                   } else {
+                       std::cout << "snode.c listening on port 8080" << std::endl;
+                   }
+               }
     );
-
+    
     app.destroy();
     
     return 0;
+}
+
+
+int simpleWebserver(int argc, char** argv) {
+    HTTPServer& app = HTTPServer::instance();
+    
+    app.serverRoot("/home/voc/projects/ServerVoc/build/html");
+    
+    app.get("/",
+            [&] (const Request& req, const Response& res) -> void {
+                std::string uri = req.requestURI();
+                
+                if (uri == "/") {
+                    uri = "/index.html";
+                }
+                
+                std::cout << uri << std::endl;
+                
+                res.sendFile(uri, [uri] (int ret) -> void {
+                    if (ret != 0) {
+                        perror(uri.c_str());
+                    }
+                });
+            });
+    
+    app.listen(8080, 
+               [] (int err) -> void {
+                   if (err != 0) {
+                       perror("Listen");
+                   } else {
+                       std::cout << "snode.c listening on port 8080" << std::endl;
+                   }
+               }
+    );
+    
+    app.destroy();
+    
+    return 0;
+}
+                
+
+
+int main(int argc, char** argv) {
+    return simpleWebserver(argc, argv);
 }
 
