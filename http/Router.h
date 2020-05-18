a6205285ff7c2453f9fe1ac49db39dedab13d18e
Volker Christian <volker.christian@fh-hagenberg.at>
2020-05-18 13:58:59 +0200

    Added a "method"-field to Route

--- http/Router.h
+++ http/Router.h
@@ -12,7 +12,7 @@ class Router;
 
 class Route {
 public:
-    Route(const Router* parent, const std::string& path) : parent(parent), path(path) {}
+    Route(const Router* parent, const std::string& method, const std::string& path) : parent(parent), method(method), path(path) {}
     virtual ~Route() {}
     
     virtual bool dispatch(const std::string& method, const std::string& mpath, const Request& req, const Response& res) const = 0;
@@ -20,6 +20,7 @@ public:
     
 protected:
     const Router* parent;
+    const std::string method;
     const std::string path;
     
 friend class Router;
@@ -28,7 +29,7 @@ friend class Router;
 
 class RouterRoute : public Route {
 public:
-    RouterRoute(const Router* parent, std::string path, Router& router) : Route(parent, path), router(router) {}
+    RouterRoute(const Router* parent, const std::string& method, std::string path, Router& router) : Route(parent, method, path), router(router) {}
     
     virtual bool dispatch(const std::string& method, const std::string& mpath, const Request& req, const Response& res) const;
     
@@ -39,7 +40,7 @@ private:
 
 class DispatcherRoute : public Route {
 public:
-    DispatcherRoute(const Router* parent, const std::string& path, const std::function<void (const Request& req, const Response& res)>& dispatcher): Route(parent, path), dispatcher(dispatcher) {}
+    DispatcherRoute(const Router* parent, const std::string& method, const std::string& path, const std::function<void (const Request& req, const Response& res)>& dispatcher): Route(parent, method, path), dispatcher(dispatcher) {}
     
     virtual bool dispatch(const std::string& method, const std::string& mpath, const Request& req, const Response& res) const;
     
@@ -50,7 +51,7 @@ private:
 
 class MiddlewareRoute : public Route {
 public:
-    MiddlewareRoute(const Router* parent, const std::string& path, const std::function<void (const Request& req, const Response& res, const std::function<void (void)>& next)>& dispatcher): Route (parent, path), dispatcher(dispatcher) {}
+    MiddlewareRoute(const Router* parent, const std::string& method, const std::string& path, const std::function<void (const Request& req, const Response& res, const std::function<void (void)>& next)>& dispatcher): Route (parent, method, path), dispatcher(dispatcher) {}
     
     virtual bool dispatch(const std::string& method, const std::string& mpath, const Request& req, const Response& res) const;
     
@@ -61,22 +62,22 @@ private:
 
 #define REQUESTMETHOD(METHOD) \
 void METHOD(const std::string& path, const std::function<void (const Request& req, const Response& res)>& dispatcher) { \
-    routes[#METHOD].push_back(new DispatcherRoute(this, path, dispatcher));\
+    routes[#METHOD].push_back(new DispatcherRoute(this, #METHOD, path, dispatcher));\
 };\
 \
 void METHOD(const std::string& path, Router& router) { \
-    routes[#METHOD].push_back(new RouterRoute(this, path, router));\
+    routes[#METHOD].push_back(new RouterRoute(this, #METHOD, path, router));\
 };\
 \
 void METHOD(const std::string& path, const std::function<void (const Request& req, const Response& res, const std::function<void (void)>& next)>& dispatcher) { \
-    mroutes[#METHOD].push_back(new MiddlewareRoute(this, path, dispatcher)); \
+    mroutes[#METHOD].push_back(new MiddlewareRoute(this, #METHOD, path, dispatcher)); \
 };
 
 
 class Router : public Route
 {
 public:
-    Router() : Route(0, "") {}
+    Router() : Route(0, "use", "") {}
     ~Router();
     
     REQUESTMETHOD(use);
