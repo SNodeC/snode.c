c84d324077c223877037ef7b7666d5ef4fae81a4
Volker Christian <volker.christian@fh-hagenberg.at>
2020-05-18 14:30:11 +0200

    New targets added

---
+++ posttest.cpp
@@ -0,0 +1,88 @@
+#include <iostream>
+
+#include <string.h>
+#include <time.h>
+
+#include "Request.h"
+#include "Response.h"
+#include "SingleshotTimer.h"
+#include "ContinousTimer.h"
+#include "HTTPServer.h"
+
+#include "httputils.h"
+         
+                
+int testPost(int argc, char* argv[]) {
+    HTTPServer& app = HTTPServer::instance();
+    
+    app.get("/",
+            [&] (const Request& req, const Response& res) -> void {
+                res.send(
+                    "<html>"\
+                        "<head>"\
+                            "<style>"\
+                                "main {"\
+                                    "min-height: 30em;"\
+                                    "padding: 3em;"\
+                                    "background-image: repeating-radial-gradient( circle at 0 0, #fff, #ddd 50px);"\
+                                "}"\
+                                "input[type=\"file\"] {"\
+                                    "display: block;"\
+                                    "margin: 2em;"\
+                                    "padding: 2em;"\
+                                    "border: 1px dotted;"\
+                                "}"\
+                            "</style>"\
+                        "</head>"\
+                        "<body>"\
+                            "<h1>Datei-Upload mit input type=\"file\"</h1>"\
+                            "<main>"\
+                                "<h2>Schicken Sie uns was Schickes!</h2>"\
+                                "<form method=\"post\" enctype=\"multipart/form-data\">"\
+                                    "<label> Wählen Sie eine Textdatei (*.txt, *.html usw.) von Ihrem Rechner aus."\
+                                        "<input name=\"datei\" type=\"file\" size=\"50\" accept=\"text/*\">"\
+                                    "</label>"\
+                                    "<button>… und ab geht die Post!</button>"\
+                                "</form>"\
+                            "</main>"\
+                        "</body>"\
+                    "</html>");
+            });
+    
+    app.post("/", 
+             [&] (const Request& req, const Response& res) -> void {
+                 std::cout << "Content-Type: " << req.header("Content-Type") << std::endl;
+                 std::cout << "Content-Length: " << req.header("Content-Length") << std::endl;
+                 char* body = new char[std::stoi(req.header("Content-Length")) + 1];
+                 memcpy(body, req.body, std::stoi(req.header("Content-Length")));
+                 body[std::stoi(req.header("Content-Length"))] = 0;
+                 
+                 std::cout << "Body: " << std::endl;
+                 std::cout << body << std::endl;
+                 res.send(
+                    "<html>"\
+                        "<body>"\
+                            "<h1>Thank you</h1>"\
+                        "</body>"\
+                    "</html>"
+                );
+             });
+    
+    app.listen(8080, 
+               [] (int err) -> void {
+                   if (err != 0) {
+                       perror("Listen");
+                   } else {
+                       std::cout << "snode.c listening on port 8080" << std::endl;
+                   }
+               }
+    );
+    
+    return 0;
+}
+                
+
+int main(int argc, char** argv) {
+    return testPost(argc, argv);
+}
+
---
+++ simpleserver.cpp
@@ -0,0 +1,133 @@
+#include <iostream>
+
+#include <string.h>
+#include <time.h>
+
+#include "Request.h"
+#include "Response.h"
+#include "SingleshotTimer.h"
+#include "ContinousTimer.h"
+#include "HTTPServer.h"
+
+#include "httputils.h"
+
+
+int simpleWebserver(int argc, char** argv) {
+    HTTPServer& app = HTTPServer::instance();
+    
+    Router router;
+    
+    router.get("/search", 
+              [&] (const Request& req, const Response& res) -> void {
+                  
+                  std::string host = req.header("Host");
+                  
+                  //                std::cout << "Host: " << host << std::endl;
+                  
+                  std::string uri = req.originalUrl;
+                  
+                  //                std::cout << "RHeader: " << req.header("Accept") << std::endl;
+                  
+                  std::cout << "Uri: " << uri << std::endl;
+                  
+                  if (uri == "/") {
+                      res.redirect("/index.html");
+                  } else {
+                      //                    std::cout << uri << std::endl;
+                      
+                      if (req.bodySize() != 0) {
+                          std::cout << "Body: " << req.body << std::endl;
+                      }
+                      
+                      res.sendFile(uri, [uri] (int ret) -> void {
+                          if (ret != 0) {
+                              std::cerr << uri << ": " << strerror(ret) << std::endl;
+                          }
+                      });
+                  }
+              });
+    
+    app.serverRoot("/home/voc/projects/ServerVoc/build/html");
+    
+    app.get("/",
+            [&] (const Request& req, const Response& res) -> void {
+                
+                std::string host = req.header("Host");
+                
+//                std::cout << "Host: " << host << std::endl;
+                
+                std::string uri = req.originalUrl;
+                
+//                std::cout << "RHeader: " << req.header("Accept") << std::endl;
+                
+                std::cout << "Uri: " << uri << std::endl;
+                
+                if (uri == "/") {
+                    res.redirect("/index.html");
+                } else {
+//                    std::cout << uri << std::endl;
+                    
+                    if (req.bodySize() != 0) {
+                        std::cout << "Body: " << req.body << std::endl;
+                    }
+                    
+                    res.sendFile(uri, [uri] (int ret) -> void {
+                        if (ret != 0) {
+                            std::cerr << uri << ": " << strerror(ret) << std::endl;
+                        }
+                    });
+                }
+            });
+    app.get("/", router);
+    
+    /*
+    app.get("/search", 
+            [&] (const Request& req, const Response& res) -> void {
+                
+                std::string host = req.header("Host");
+                
+                //                std::cout << "Host: " << host << std::endl;
+                
+                std::string uri = req.originalUrl;
+                
+                //                std::cout << "RHeader: " << req.header("Accept") << std::endl;
+                
+                std::cout << "Uri: " << uri << std::endl;
+                
+                if (uri == "/") {
+                    res.redirect("/index.html");
+                } else {
+                    //                    std::cout << uri << std::endl;
+                    
+                    if (req.bodySize() != 0) {
+                        std::cout << "Body: " << req.body << std::endl;
+                    }
+                    
+                    res.sendFile(uri, [uri] (int ret) -> void {
+                        if (ret != 0) {
+                            std::cerr << uri << ": " << strerror(ret) << std::endl;
+                        }
+                    });
+                }
+            });*/
+    
+    app.listen(8080, 
+               [] (int err) -> void {
+                   if (err != 0) {
+                       perror("Listen");
+                   } else {
+                       std::cout << "snode.c listening on port 8080" << std::endl;
+                   }
+               }
+    );
+    
+    app.destroy();
+    
+    return 0;
+}
+
+
+int main(int argc, char** argv) {
+    return simpleWebserver(argc, argv);
+}
+
