43d1aaeabf9d3d97af8492721da6a42e8554fb9b
Volker Christian <volker.christian@fh-hagenberg.at>
2020-05-23 18:30:32 +0200

    Reformat source

--- simpleserver.cpp
+++ simpleserver.cpp
@@ -5,8 +5,8 @@
 int simpleWebserver(int argc, char** argv) {
     WebApp& app = WebApp::instance("/home/voc/projects/ServerVoc/build/html");
     app.get("/",
-            [&] (const Request& req, const Response& res) -> void {
-                std::string uri = req.originalUrl;
+    [&] (const Request& req, const Response& res) -> void {
+        std::string uri = req.originalUrl;
 //                std::cout << "Connection: " << req.header("Connection") << std::endl;
 //                std::cout << "URL: " << uri << std::endl;
 //                std::cout << "Cookie: " << req.cookie("rootcookie") << std::endl;
@@ -14,48 +14,47 @@ int simpleWebserver(int argc, char** argv) {
 //                res.clearCookie("rootcookie");
 //                res.clearCookie("rootcookie");
 //                res.clearCookie("searchcookie", {{"Path", "/search"}});
-                if (uri == "/") {
-                    res.redirect("/index.html");
-                } else if (uri == "/end") {
-                    app.stop();
-                } else {
-                    res.sendFile(uri);
-                }
-            });
-    
+        if (uri == "/") {
+            res.redirect("/index.html");
+        } else if (uri == "/end") {
+            app.stop();
+        } else {
+            res.sendFile(uri);
+        }
+    });
+
     Router router;
-    router.get("/search", 
-               [&] (const Request& req, const Response& res) -> void {
-                   std::cout << "URL: " << req.originalUrl << std::endl;
-                   std::cout << "Cookie: " << req.cookie("searchcookie") << std::endl;
-                   res.sendFile(req.originalUrl);
-               });
-    
+    router.get("/search",
+    [&] (const Request& req, const Response& res) -> void {
+        std::cout << "URL: " << req.originalUrl << std::endl;
+        std::cout << "Cookie: " << req.cookie("searchcookie") << std::endl;
+        res.sendFile(req.originalUrl);
+    });
+
     app.get("/", router);
-    
-    app.listen(8080, 
-               [&] (int err) -> void {
-                   if (err != 0) {
-                       perror("Listen");
-                   } else {
-                       std::cout << "snode.c listening on port 8080" << std::endl;
-                       
-                       app.sslListen(443, 
-                                     [] (int err) -> void {
-                                         if (err != 0) {
-                                             perror("Listen");
-                                         } else {
-                                             std::cout << "snode.c listening on port 443" << std::endl;
-                                         }
-                                     }
-                       );
-                   }
-               }
-    );
-    
-    
+
+    app.listen(8080,
+    [&] (int err) -> void {
+        if (err != 0) {
+            perror("Listen");
+        } else {
+            std::cout << "snode.c listening on port 8080" << std::endl;
+            app.sslListen(8088,
+            [] (int err) -> void {
+                if (err != 0) {
+                    perror("Listen");
+                } else {
+                    std::cout << "snode.c listening on port 8088" << std::endl;
+                }
+            }
+                         );
+        }
+    }
+              );
+
+
     app.destroy();
-    
+
     return 0;
 }
 
