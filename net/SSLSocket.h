ce46b65a21359eb66dcab1d5ef522cb6bdf27cf4
Volker Christian <volker.christian@fh-hagenberg.at>
2020-05-23 12:58:17 +0200

    Start of SSL Socket implementation

---
+++ net/SSLSocket.h
@@ -0,0 +1,41 @@
+#ifndef SSLSOCKET_H
+#define SSLSOCKET_H
+
+#ifndef DOXYGEN_SHOULD_SKIP_THIS
+
+#include <openssl/rsa.h>
+#include <openssl/crypto.h>
+#include <openssl/x509.h>
+#include <openssl/pem.h>
+#include <openssl/ssl.h>
+#include <openssl/err.h>
+
+#endif /* DOXYGEN_SHOULD_SKIP_THIS */
+
+#include "SocketBase.h"
+
+
+class SSLSocket : public SocketBase
+{
+public:
+    virtual void setFd(int fd);
+    
+    virtual ~SSLSocket();
+    
+protected:
+    SSLSocket();
+    SSLSocket(int fd);
+    
+    ssize_t recv(void *buf, size_t len, int flags);
+    ssize_t send(const void *buf, size_t len, int flags);
+    
+private:
+    static const SSL_METHOD* init1();
+    static SSL_CTX* init2();
+    static SSL_CTX* ctx;
+    static const SSL_METHOD* meth;
+    SSL* ssl;
+    int err;
+};
+
+#endif // SSLSOCKET_H
--- net/Socket.cpp
+++ net/Socket.cpp
@@ -13,30 +13,17 @@
 Socket::Socket() {}
 
 
-Socket::Socket(int fd) : Descriptor(fd) {
+Socket::Socket(int fd) : SocketBase() {
     this->setFd(fd);
 }
 
 
 Socket::~Socket() {
-    ::shutdown(this->getFd(), SHUT_RDWR);
 }
 
 
 void Socket::setFd(int fd) {
-    Descriptor::setFd(fd);
-}
-
-
-void Socket::open(const std::function<void (int errnum)>& onError) {
-    int fd = ::socket(AF_INET, SOCK_STREAM, 0);
-    
-    if (fd >= 0) {
-        this->setFd(fd);
-        onError(0);
-    } else {
-        onError(errno);
-    }
+    SocketBase::setFd(fd);
 }
 
 
@@ -47,29 +34,4 @@ ssize_t Socket::recv(void *buf, size_t len, int flags) {
 
 ssize_t Socket::send(const void *buf, size_t len, int flags) {
     return ::send(this->getFd(), buf, len, flags);
-}
-
-
-InetAddress& Socket::getLocalAddress() {
-    return localAddress;
-}
-
-
-void Socket::bind(InetAddress& localAddress, const std::function<void (int errnum)>& onError) {
-    int ret = 0;
-    
-    socklen_t addrlen = sizeof(struct sockaddr_in);
-    
-    ret = ::bind(this->getFd(), (struct sockaddr*) &localAddress.getSockAddr(), addrlen);
-    
-    if (ret < 0) {
-        onError(errno);
-    } else {
-        onError(0);
-    }
-}
-
-
-void Socket::setLocalAddress(const InetAddress& localAddress) {
-    this->localAddress = localAddress;
 }
