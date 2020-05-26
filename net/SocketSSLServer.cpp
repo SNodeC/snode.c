d39e72d60a732bb3c65aa59421257cee63dda63c
Volker Christian <volker.christian@fh-hagenberg.at>
2020-05-26 19:13:04 +0200

    Support of password encrypted key-files

--- net/SocketSSLServer.cpp
+++ net/SocketSSLServer.cpp
@@ -1,5 +1,9 @@
 #ifndef DOXYGEN_SHOULD_SKIP_THIS
 
+#include <string.h>
+
+#include <iostream>
+
 #endif /* DOXYGEN_SHOULD_SKIP_THIS */
 
 #include "SocketSSLServer.h"
@@ -34,9 +38,9 @@ SocketSSLServer::~SocketSSLServer() {
 }
 
 
-void SocketSSLServer::listen(in_port_t port, int backlog, const std::string& cert, const std::string& key, const std::string& password, const std::function<void (int err)>& onError) {
+void SocketSSLServer::listen(in_port_t port, int backlog, const std::string& certChain, const std::string& keyPEM, const std::string& password, const std::function<void (int err)>& onError) {
     SocketServerBase<SocketSSLConnection>::listen(port, backlog, 
-        [this, &cert, &key, &password, onError] (int err) -> void {
+        [this, &certChain, &keyPEM, &password, onError] (int err) -> void {
             SSL_load_error_strings();
             SSL_library_init();
             OpenSSL_add_ssl_algorithms();
@@ -45,18 +49,32 @@ void SocketSSLServer::listen(in_port_t port, int backlog, const std::string& cer
             if (!ctx) {
                 ERR_print_errors_fp(stderr);
                 err = ERR_get_error();
-            } else if (SSL_CTX_use_certificate_file(ctx, cert.c_str(), SSL_FILETYPE_PEM) <= 0) {
-                ERR_print_errors_fp(stderr);
-                err = ERR_get_error();
-            } else if (SSL_CTX_use_PrivateKey_file(ctx, key.c_str(), SSL_FILETYPE_PEM) <= 0) {
-                ERR_print_errors_fp(stderr);
-                err = ERR_get_error();
-            } else if (!SSL_CTX_check_private_key(ctx)) {
-//                fprintf(stderr,"Private key does not match the certificate public key\n");
-                err = ERR_get_error();
+            } else {
+                SSL_CTX_set_default_passwd_cb(ctx, SocketSSLServer::passwordCallback);
+                SSL_CTX_set_default_passwd_cb_userdata(ctx, ::strdup(password.c_str()));
+                if (SSL_CTX_use_certificate_chain_file(ctx, certChain.c_str()) <= 0) {
+                    ERR_print_errors_fp(stderr);
+                    err = ERR_get_error();
+                } else if (SSL_CTX_use_PrivateKey_file(ctx, keyPEM.c_str(), SSL_FILETYPE_PEM) <= 0) {
+                    ERR_print_errors_fp(stderr);
+                    err = ERR_get_error();
+                } else if (!SSL_CTX_check_private_key(ctx)) {
+                    err = ERR_get_error();
+                }
             }
             
             onError(err);
         }
     );
+}
+
+
+int SocketSSLServer::passwordCallback(char *buf, int size, int rwflag, void *u) {
+    ::strncpy(buf, (char*) u, size);
+    buf[size - 1] = '\0';
+    
+    ::memset(u, 0, ::strlen((char*) u)); // garble password
+    ::free(u);
+    
+    return ::strlen(buf);
 }
