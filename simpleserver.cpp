5ca2493fa4fd79f35566dad05fe82985dab0cc5b
Volker Christian <volker.christian@fh-hagenberg.at>
2020-05-27 13:05:08 +0200

    Use two WebApp objects. One for legacy connections and one for ssl connections.

--- simpleserver.cpp
+++ simpleserver.cpp
@@ -6,61 +6,72 @@
 #endif /* DOXYGEN_SHOULD_SKIP_THIS */
 
 
-int simpleWebserver(int argc, char** argv) {
-    WebApp& app = WebApp::instance("/home/voc/projects/ServerVoc/build/html");
+int simpleWebserver (int argc, char** argv)
+{
+    WebApp& legacyApp = WebApp::instance("/home/voc/projects/ServerVoc/build/html/");
+    legacyApp.use("/",
+        [] (const Request& req, const Response& res, const std::function<void ( void ) >& next) {
+            std::cout << "Redirect: " << "https://localhost:8088" + req.originalUrl << std::endl;
+            res.redirect("https://localhost:8088" + req.originalUrl);
+        }
+    );
+
     
-    app.use("/",
-        [&] (const Request& req, const Response& res, const std::function<void (void)>& next) {
-            res.set("Connection", "Keep-Alive");
+    WebApp& sslApp = WebApp::instance("/home/voc/projects/ServerVoc/build/html/");
+    sslApp.use("/",
+        [&] (const Request& req, const Response& res, const std::function<void ( void ) >& next) {
+            res.set ( "Connection", "Keep-Alive" );
             next();
-        });
-                
-    
-    app.get("/",
+        }
+    );
+
+
+    sslApp.get("/",
         [&] (const Request& req, const Response& res) -> void {
             std::string uri = req.originalUrl;
-                std::cout << "URL: " << uri << std::endl;
-                std::cout << "Cookie: " << req.cookie("rootcookie") << std::endl;
-                res.cookie("searchcookie", "cookievalue", {{"Max-Age", "3600"}, {"Path", "/search"}});
+            std::cout << "URL: " << uri << std::endl;
+            std::cout << "Cookie: " << req.cookie ( "rootcookie" ) << std::endl;
+            res.cookie ("searchcookie", "cookievalue", {{"Max-Age", "3600"}, {"Path", "/search"}});
 //                res.clearCookie("rootcookie");
 //                res.clearCookie("rootcookie");
 //                res.clearCookie("searchcookie", {{"Path", "/search"}});
             if (uri == "/") {
-                res.redirect("/index.html");
+                res.redirect ("/index.html");
             } else if (uri == "/end") {
-                app.stop();
+                WebApp::stop();
             } else {
                 res.sendFile(uri);
             }
         }
     );
 
+    
     Router router;
     router.get("/search",
         [&] (const Request& req, const Response& res) -> void {
             std::cout << "URL: " << req.originalUrl << std::endl;
-            std::cout << "Cookie: " << req.cookie("searchcookie") << std::endl;
-            res.sendFile(req.originalUrl);
+            std::cout << "Cookie: " << req.cookie ( "searchcookie" ) << std::endl;
+            res.sendFile (req.originalUrl);
         }
     );
 
-    app.get("/", router);
-    
-    #define CERTF "/home/voc/projects/ServerVoc/certs/Volker_Christian_-_Web_-_snode.c.pem"
-    #define KEYF "/home/voc/projects/ServerVoc/certs/Volker_Christian_-_Web_-_snode.c.key.encrypted.pem"
-    
-    app.listen(8080, 
-        [&app] (int err) -> void {
+    sslApp.get("/", router);
+
+#define CERTF "/home/voc/projects/ServerVoc/certs/Volker_Christian_-_Web_-_snode.c.pem"
+#define KEYF "/home/voc/projects/ServerVoc/certs/Volker_Christian_-_Web_-_snode.c.key.encrypted.pem"
+
+    sslApp.sslListen(8088, CERTF, KEYF, "snode.c",
+        [&legacyApp] (int err) -> void {
             if (err != 0) {
                 perror("Listen");
             } else {
-                std::cout << "snode.c listening on port 8080" << std::endl;
-                app.sslListen(8088, CERTF, KEYF, "snode.c",
+                std::cout << "snode.c listening on port 8088" << std::endl;
+                legacyApp.listen(8080,
                     [] (int err) -> void {
                         if (err != 0) {
                             perror("Listen");
                         } else {
-                            std::cout << "snode.c listening on port 8088" << std::endl;
+                            std::cout << "snode.c listening on port 8080" << std::endl;
                         }
                     }
                 );
@@ -68,12 +79,14 @@ int simpleWebserver(int argc, char** argv) {
         }
     );
 
-    app.destroy();
+    legacyApp.destroy();
+    sslApp.destroy();
 
     return 0;
 }
 
 
-int main(int argc, char** argv) {
+int main(int argc, char** argv)
+{
     return simpleWebserver(argc, argv);
 }
