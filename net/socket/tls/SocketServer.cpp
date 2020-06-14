967e6f7a2d60e9edc95d79ab666abace6d1507f7
Volker Christian <volker.christian@fh-hagenberg.at>
2020-06-14 21:08:53 +0200

    Fix error handling

--- net/socket/tls/SocketServer.cpp
+++ net/socket/tls/SocketServer.cpp
@@ -52,29 +52,32 @@ namespace tls {
     void SocketServer::listen(in_port_t port, int backlog, const std::string& certChain, const std::string& keyPEM,
                               const std::string& password, const std::function<void(int err)>& onError) {
         SocketServerBase<SocketConnection>::listen(port, backlog, [this, &certChain, &keyPEM, &password, onError](int err) -> void {
-            SSL_load_error_strings();
-            SSL_library_init();
-            OpenSSL_add_ssl_algorithms();
+            if (!err) {
+                SSL_load_error_strings();
+                SSL_library_init();
+                OpenSSL_add_ssl_algorithms();
 
-            ctx = SSL_CTX_new(TLS_server_method());
-            if (!ctx) {
-                ERR_print_errors_fp(stderr);
-                err = ERR_get_error();
-            } else {
-                SSL_CTX_set_default_passwd_cb(ctx, SocketServer::passwordCallback);
-                SSL_CTX_set_default_passwd_cb_userdata(ctx, ::strdup(password.c_str()));
-                if (SSL_CTX_use_certificate_chain_file(ctx, certChain.c_str()) <= 0) {
+                ctx = SSL_CTX_new(TLS_server_method());
+                if (!ctx) {
                     ERR_print_errors_fp(stderr);
                     err = ERR_get_error();
-                } else if (SSL_CTX_use_PrivateKey_file(ctx, keyPEM.c_str(), SSL_FILETYPE_PEM) <= 0) {
-                    ERR_print_errors_fp(stderr);
-                    err = ERR_get_error();
-                } else if (!SSL_CTX_check_private_key(ctx)) {
-                    err = ERR_get_error();
+                } else {
+                    SSL_CTX_set_default_passwd_cb(ctx, SocketServer::passwordCallback);
+                    SSL_CTX_set_default_passwd_cb_userdata(ctx, ::strdup(password.c_str()));
+                    if (SSL_CTX_use_certificate_chain_file(ctx, certChain.c_str()) <= 0) {
+                        ERR_print_errors_fp(stderr);
+                        err = ERR_get_error();
+                    } else if (SSL_CTX_use_PrivateKey_file(ctx, keyPEM.c_str(), SSL_FILETYPE_PEM) <= 0) {
+                        ERR_print_errors_fp(stderr);
+                        err = ERR_get_error();
+                    } else if (!SSL_CTX_check_private_key(ctx)) {
+                        err = ERR_get_error();
+                    }
                 }
+                onError(err);
+            } else {
+                onError(err);
             }
-
-            onError(err);
         });
     }
 
