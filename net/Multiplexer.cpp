595ed873f3216b571673f9b3ebd5c20d864e087d
Volker Christian <volker.christian@fh-hagenberg.at>
2020-07-24 23:55:13 +0200

    Polish

--- net/Multiplexer.cpp
+++ net/Multiplexer.cpp
@@ -64,7 +64,7 @@ void Multiplexer::tick() {
         PLOG(ERROR) << "select";
         stop();
     }
-    
+
     managedReader.unobserveStopedDescriptors();
     managedWriter.unobserveStopedDescriptors();
     managedExceptions.unobserveStopedDescriptors();
