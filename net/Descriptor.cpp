e1ae8a4735b87a94e30b5e9557c2d8f5f7b1bb2c
Volker Christian <volker.christian@fh-hagenberg.at>
2020-07-28 10:46:45 +0200

    Using SOCK_NONBLOCK in socket()-call and in accept4()-call

--- net/Descriptor.cpp
+++ net/Descriptor.cpp
@@ -1,6 +1,5 @@
 #ifndef DOXYGEN_SHOULD_SKIP_THIS
 
-#include <fcntl.h>
 #include <iostream>
 #include <unistd.h>
 
@@ -16,16 +15,6 @@ Descriptor::~Descriptor() {
 
 void Descriptor::attachFd(int fd) {
     this->fd = fd;
-}
-
-
-void Descriptor::setNonBlocking() {
-    int flags = fcntl(fd, F_GETFL, 0);
-    if (flags < 0) {
-    }
-
-    if (fcntl(fd, F_SETFL, flags | O_NONBLOCK) < 0) {
-    }
 }
 
 
