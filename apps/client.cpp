300ec80fbb0f00fd9da5cdebec918e8145120853
Volker Christian <volker.christian@fh-hagenberg.at>
2020-07-29 10:29:26 +0200

    Move example applications into directory apps

--- apps/client.cpp
+++ apps/client.cpp
@@ -86,8 +86,8 @@ int main(int argc, char* argv[]) {
 
     tls::SocketClient sc = tlsClient();
     legacy::SocketClient lc = legacyClient();
-    
-    
+
+
     lc.connect("localhost", 8080, [](int err) -> void {
         if (err) {
             std::cout << "Connect: " << strerror(err) << std::endl;
@@ -95,7 +95,7 @@ int main(int argc, char* argv[]) {
             std::cout << "Connected" << std::endl;
         }
     });
-    
+
     sc.connect("localhost", 8088, [](int err) -> void {
         if (err) {
             std::cout << "Connect: " << strerror(err) << std::endl;
--- apps/simpleserver.cpp
+++ apps/simpleserver.cpp
@@ -10,8 +10,8 @@
 #endif /* DOXYGEN_SHOULD_SKIP_THIS */
 
 
-#define MIDDLEWARE(req, res, next) [&](Request&(req), Response&(res), const std::function<void(void)>&(next)) -> void
-#define APPLICATION(req, res) [&](Request&(req), Response&(res)) -> void
+#define MIDDLEWARE(req, res, next) [&](Request & (req), Response & (res), const std::function<void(void)>&(next)) -> void
+#define APPLICATION(req, res) [&](Request & (req), Response & (res)) -> void
 
 #define CERTF "/home/voc/projects/ServerVoc/certs/calisto.home.vchrist.at_-_snode.c.pem"
 #define KEYF "/home/voc/projects/ServerVoc/certs/Volker_Christian_-_Web_-_snode.c.key.encrypted.pem"
@@ -24,33 +24,38 @@ int main(int argc, char** argv) {
 
     Router router;
     router
-        .use("/", MIDDLEWARE(req, res, next) {
-            res.set("Connection", "Keep-Alive");
-            next();
-        })
-        .get("/", APPLICATION(req, res) {
-            VLOG(0) << "URL: " + req.originalUrl;
-            if (req.originalUrl == "/") {
-                res.redirect(308, "/index.html");
-            } else if (req.originalUrl == "/end") {
-                res.send("Bye, bye!\n");
-                WebApp::stop();
-            } else {
+        .use(
+            "/",
+            MIDDLEWARE(req, res, next) {
+                res.set("Connection", "Keep-Alive");
+                next();
+            })
+        .get(
+            "/",
+            APPLICATION(req, res) {
+                VLOG(0) << "URL: " + req.originalUrl;
+                if (req.originalUrl == "/") {
+                    res.redirect(308, "/index.html");
+                } else if (req.originalUrl == "/end") {
+                    res.send("Bye, bye!\n");
+                    WebApp::stop();
+                } else {
+                    res.sendFile(req.originalUrl, [&req](int ret) -> void {
+                        if (ret != 0) {
+                            PLOG(ERROR) << req.originalUrl;
+                        }
+                    });
+                }
+            })
+        .get(
+            "/search", APPLICATION(req, res) {
+                VLOG(0) << "URL: " + req.originalUrl;
                 res.sendFile(req.originalUrl, [&req](int ret) -> void {
                     if (ret != 0) {
                         PLOG(ERROR) << req.originalUrl;
                     }
                 });
-            }
-        })
-        .get("/search", APPLICATION(req, res) {
-            VLOG(0) << "URL: " + req.originalUrl;
-            res.sendFile(req.originalUrl, [&req](int ret) -> void {
-                if (ret != 0) {
-                    PLOG(ERROR) << req.originalUrl;
-                }
             });
-        });
 
     legacy::WebApp legacyApp(SERVERROOT);
     legacyApp.use("/", router);
