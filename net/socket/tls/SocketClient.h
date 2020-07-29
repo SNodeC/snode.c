543578629fd520d32bd6843f2da51c7baf32b759
Volker Christian <volker.christian@fh-hagenberg.at>
2020-07-29 15:48:16 +0200

    Prohibit using the connect() methods of base class

--- net/socket/tls/SocketClient.h
+++ net/socket/tls/SocketClient.h
@@ -29,6 +29,9 @@ namespace tls {
     private:
         SSL_CTX* ctx;
 
+    protected:
+        using ::SocketClient<tls::SocketConnection>::connect;
+
     private:
         std::function<void(tls::SocketConnection* cs)> onConnect;
         std::function<void(tls::SocketConnection* cs)> onDisconnect;
