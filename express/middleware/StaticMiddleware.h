e918b7dbc0dc1007cbc7d78992d28cb3e04114a4
Volker Christian <volker.christian@fh-hagenberg.at>
2020-08-17 21:44:40 +0200

    Add middleware

---
+++ express/middleware/StaticMiddleware.h
@@ -0,0 +1,32 @@
+/*
+ * snode.c - a slim toolkit for network communication
+ * Copyright (C) 2020  Volker Christian <me@vchrist.at>
+ *
+ * This program is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with this program.  If not, see <http://www.gnu.org/licenses/>.
+ */
+
+#ifndef STATICMIDDLEWARE_H
+#define STATICMIDDLEWARE_H
+
+#include "Router.h"
+
+class StaticMiddleware : public Router {
+public:
+    explicit StaticMiddleware(const std::string& root);
+
+protected:
+    std::string root;
+};
+
+#endif // STATICMIDDLEWARE_H
