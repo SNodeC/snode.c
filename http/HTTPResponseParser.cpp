a6a77d265c3554a2bd24954553dab83f50d2d7c5
Volker Christian <volker.christian@fh-hagenberg.at>
2020-08-28 10:48:39 +0200

    Add preliminary HTTPResponseParser

---
+++ http/HTTPResponseParser.cpp
@@ -0,0 +1,138 @@
+/*
+ * snode.c - a slim toolkit for network communication
+ * Copyright (C) 2020  Volker Christian <me@vchrist.at>
+ *
+ * This program is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with this program.  If not, see <http://www.gnu.org/licenses/>.
+ */
+
+#ifndef DOXYGEN_SHOULD_SKIP_THIS
+
+#include <easylogging++.h>
+#include <tuple> // for tie, tuple
+
+#endif /* DOXYGEN_SHOULD_SKIP_THIS */
+
+#include "HTTPResponseParser.h"
+#include "httputils.h"
+
+namespace http {
+
+    HTTPResponseParser::HTTPResponseParser(
+        const std::function<void(const std::string&, const std::string&, const std::string&)>& onResponse,
+        const std::function<void(const std::map<std::string, std::string>&, const std::map<std::string, std::string>&)>& onHeader,
+        const std::function<void(char*, size_t)>& onContent, const std::function<void(void)>& onParsed,
+        const std::function<void(int status, const std::string& reason)>& onError)
+        : onResponse(onResponse)
+        , onHeader(onHeader)
+        , onContent(onContent)
+        , onParsed(onParsed)
+        , onError(onError) {
+    }
+
+    HTTPResponseParser::HTTPResponseParser(
+        const std::function<void(const std::string&, const std::string&, const std::string&)>&& onResponse,
+        const std::function<void(const std::map<std::string, std::string>&, const std::map<std::string, std::string>&)>&& onHeader,
+        const std::function<void(char*, size_t)>&& onContent, const std::function<void(void)>&& onParsed,
+        const std::function<void(int status, const std::string& reason)>&& onError)
+        : onResponse(onResponse)
+        , onHeader(onHeader)
+        , onContent(onContent)
+        , onParsed(onParsed)
+        , onError(onError) {
+    }
+
+    void HTTPResponseParser::reset() {
+        HTTPParser::reset();
+        httpVersion.clear();
+        statusCode.clear();
+        reason.clear();
+        cookies.clear();
+    }
+
+    enum HTTPParser::PAS HTTPResponseParser::parseStartLine([[maybe_unused]] std::string& line) {
+        enum HTTPParser::PAS PAS = HTTPParser::PAS::HEADER;
+
+        if (!line.empty()) {
+            std::string remaining;
+
+            std::tie(httpVersion, remaining) = httputils::str_split(line, ' '); // if split not found second will be empty
+            std::tie(statusCode, remaining) = httputils::str_split(remaining, ' ');
+            std::tie(reason, std::ignore) = httputils::str_split(remaining, ' ');
+
+            onResponse(httpVersion, statusCode, reason);
+        } else {
+            PAS = parsingError(400, "Response-line empty");
+        }
+        return PAS;
+    }
+
+    enum HTTPParser::PAS HTTPResponseParser::parseHeader() {
+        for (auto& [field, value] : HTTPParser::headers) {
+            VLOG(1) << "++ Parse header field: " << field << " = " << value;
+            if (field != "set-cookie") {
+                if (field == "content-length") {
+                    HTTPParser::contentLength = std::stoi(value);
+                }
+            } else {
+                std::string cookieLine = value;
+
+                while (!cookieLine.empty()) {
+                    /*
+                    std::string cookie;
+                    std::tie(cookie, cookieLine) = httputils::str_split(cookieLine, ';');
+
+                    std::string name;
+                    std::string value;
+                    std::tie(name, value) = httputils::str_split(cookie, '=');
+
+                    httputils::str_trimm(name);
+                    httputils::str_trimm(value);
+
+                    VLOG(1) << "++ Cookie: " << name << " = " << value;
+
+                    cookies.insert({name, value});
+                    */
+                }
+            }
+        }
+
+        //        onHeader(HTTPParser::headers, cookies);
+
+        enum HTTPParser::PAS PAS = HTTPParser::PAS::BODY;
+        if (contentLength == 0) {
+            parsingFinished();
+            PAS = PAS::FIRSTLINE;
+        }
+
+        return PAS;
+    }
+    enum HTTPParser::PAS HTTPResponseParser::parseContent([[maybe_unused]] char* content, [[maybe_unused]] size_t size) {
+        onContent(content, size);
+        parsingFinished();
+
+        return PAS::FIRSTLINE;
+    }
+
+    enum HTTPParser::PAS HTTPResponseParser::parsingError([[maybe_unused]] int code, [[maybe_unused]] const std::string& reason) {
+        onError(code, reason);
+        reset();
+
+        return PAS::ERROR;
+    }
+
+    void HTTPResponseParser::parsingFinished() {
+        onParsed();
+    }
+
+} // namespace http
---
+++ http/HTTPResponseParser.h
@@ -0,0 +1,71 @@
+/*
+ * snode.c - a slim toolkit for network communication
+ * Copyright (C) 2020  Volker Christian <me@vchrist.at>
+ *
+ * This program is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with this program.  If not, see <http://www.gnu.org/licenses/>.
+ */
+
+#ifndef HTTPRESPONSEPARSER_H
+#define HTTPRESPONSEPARSER_H
+
+#ifndef DOXYGEN_SHOULD_SKIP_THIS
+
+#include <functional>
+#include <string>
+
+#endif /* DOXYGEN_SHOULD_SKIP_THIS */
+
+#include "HTTPParser.h"
+
+namespace http {
+
+    class HTTPResponseParser : public HTTPParser {
+    public:
+        HTTPResponseParser(
+            const std::function<void(const std::string&, const std::string&, const std::string&)>& onResponse,
+            const std::function<void(const std::map<std::string, std::string>&, const std::map<std::string, std::string>&)>& onHeader,
+            const std::function<void(char*, size_t)>& onContent, const std::function<void(void)>& onParsed,
+            const std::function<void(int status, const std::string& reason)>& onError);
+
+        HTTPResponseParser(
+            const std::function<void(const std::string&, const std::string&, const std::string&)>&& onResponse,
+            const std::function<void(const std::map<std::string, std::string>&, const std::map<std::string, std::string>&)>&& onHeader,
+            const std::function<void(char*, size_t)>&& onContent, const std::function<void(void)>&& onParsed,
+            const std::function<void(int status, const std::string& reason)>&& onError);
+
+        enum HTTPParser::PAS parseStartLine(std::string& line) override;
+        enum HTTPParser::PAS parseHeader() override;
+        enum HTTPParser::PAS parseContent(char* content, size_t size) override;
+        enum HTTPParser::PAS parsingError(int code, const std::string& reason) override;
+
+        void reset() override;
+
+    protected:
+        void parsingFinished();
+
+        std::string httpVersion;
+        std::string statusCode;
+        std::string reason;
+        std::map<std::string, std::string> cookies;
+
+        std::function<void(const std::string&, const std::string&, const std::string&)> onResponse;
+        std::function<void(const std::map<std::string, std::string>&, const std::map<std::string, std::string>&)> onHeader;
+        std::function<void(char*, size_t)> onContent;
+        std::function<void(void)> onParsed;
+        std::function<void(int status, const std::string& reason)> onError;
+    };
+
+} // namespace http
+
+#endif // HTTPRESPONSEPARSER_H
