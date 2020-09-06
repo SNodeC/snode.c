103352f930c9a7049a4bc7fd400fe638e95b8607
Volker Christian <volker.christian@fh-hagenberg.at>
2020-09-06 17:49:57 +0200

    Add copyright

--- net/socket/tls/ssl_utils.h
+++ net/socket/tls/ssl_utils.h
@@ -1,10 +1,32 @@
+/*
+ * snode.c - a slim toolkit for network communication
+ * Copyright (C) 2020  Volker Christian <me@vchrist.at>
+ *
+ * This program is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with this program.  If not, see <http://www.gnu.org/licenses/>.
+ */
+
 #ifndef SSL_UTILS_H
 #define SSL_UTILS_H
+
+#ifndef DOXYGEN_SHOULD_SKIP_THIS
 
 #include <any>
 #include <map>
 #include <openssl/ossl_typ.h> // for SSL_CTX
 #include <string>
+
+#endif /* DOXYGEN_SHOULD_SKIP_THIS */
 
 namespace net::socket::tls {
     unsigned long ssl_init_ctx(SSL_CTX* ctx, const std::map<std::string, std::any>& options, bool server = false);
