1eeb4804c874e0daf765ef5e7c268e64c7253e96
Volker Christian <volker.christian@fh-hagenberg.at>
2020-09-26 13:25:30 +0200

    Add verysimpleserver sources

---
+++ apps/verysimpleserver.cpp
@@ -0,0 +1,63 @@
+/*
+ * snode.c - a slim toolkit for network communication
+ * Copyright (C) 2020 Volker Christian <me@vchrist.at>
+ *
+ * This program is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU Lesser General Public License as published
+ * by the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the
+ * GNU Lesser General Public License for more details.
+ *
+ * You should have received a copy of the GNU Lesser General Public License
+ * along with this program. If not, see <http://www.gnu.org/licenses/>.
+ */
+
+#ifndef DOXYGEN_SHOULD_SKIP_THIS
+
+#include "legacy/WebApp.h"
+#include "middleware/StaticMiddleware.h"
+#include "tls/WebApp.h"
+
+#include <easylogging++.h>
+
+#endif /* DOXYGEN_SHOULD_SKIP_THIS */
+
+#define CERTF "/home/voc/projects/ServerVoc/certs/calisto.home.vchrist.at_-_snode.c_-_server.pem"
+#define KEYF "/home/voc/projects/ServerVoc/certs/Volker_Christian_-_Web_-_snode.c_-_server.key.encrypted.pem"
+#define KEYFPASS "snode.c"
+
+#define SERVERROOT "/home/voc/projects/ServerVoc/doc/html"
+
+int main(int argc, char* argv[]) {
+    express::WebApp::init(argc, argv);
+
+    express::legacy::WebApp legacyApp;
+    legacyApp.use(express::middleware::StaticMiddleware(SERVERROOT));
+
+    express::tls::WebApp tlsApp({{"certChain", CERTF}, {"keyPEM", KEYF}, {"password", KEYFPASS}});
+    tlsApp.use(express::middleware::StaticMiddleware(SERVERROOT));
+
+    legacyApp.listen(8080, [](int err) {
+        if (err != 0) {
+            PLOG(FATAL) << "listen on port 8080";
+        } else {
+            VLOG(0) << "snode.c listening on port 8080 for legacy connections";
+        }
+    });
+
+    tlsApp.listen(8088, [](int err) {
+        if (err != 0) {
+            PLOG(FATAL) << "listen on port 8088";
+        } else {
+            VLOG(0) << "snode.c listening on port 8088 for SSL/TLS connections";
+        }
+    });
+
+    express::WebApp::start();
+
+    return 0;
+}
