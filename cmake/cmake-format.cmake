eb068734f82af0564b41d9e79b407d5b6feba9ea
Volker Christian <volker.christian@fh-hagenberg.at>
2020-10-24 12:46:22 +0200

    Polish

--- cmake/cmake-format.cmake
+++ cmake/cmake-format.cmake
@@ -1,6 +1,7 @@
 # Additional targets to perform cmake-format
 
 find_program(CMAKE_FORMAT "cmake-format")
+
 if(CMAKE_FORMAT)
   file(GLOB_RECURSE CMAKELISTS_TXT_FILES ${CMAKE_SOURCE_DIR}/CMakeLists.txt
        ${CMAKE_SOURCE_DIR}/cmake/*.cmake)
