1196b611f4d636e85c17cf1da99197e957d29ac7
Volker Christian <volker.christian@fh-hagenberg.at>
2020-11-20 08:35:05 +0100

    Add l2cap server and client

---
+++ apps/l2lowlevelserver.cpp
@@ -0,0 +1,74 @@
+/*
+ * snode.c - a slim toolkit for network communication
+ * Copyright (C) 2020 Volker Christian <me@vchrist.at>
+ *
+ * This program is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU Lesser General Public License as published
+ * by the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the
+ * GNU Lesser General Public License for more details.
+ *
+ * You should have received a copy of the GNU Lesser General Public License
+ * along with this program. If not, see <http://www.gnu.org/licenses/>.
+ */
+
+#ifndef DOXYGEN_SHOULD_SKIP_THIS
+
+#include "EventLoop.h"
+#include "Logger.h"
+#include "config.h" // just for this example app
+#include "socket/bluetooth/l2cap/SocketServer.h"
+
+#endif /* DOXYGEN_SHOULD_SKIP_THIS */
+
+using namespace net::socket::bluetooth::l2cap;
+
+int main(int argc, char* argv[]) {
+    net::EventLoop::init(argc, argv);
+
+    SocketServer btServer(
+        []([[maybe_unused]] SocketServer::SocketConnection* socketConnection) -> void { // onConstruct
+        },
+        []([[maybe_unused]] SocketServer::SocketConnection* socketConnection) -> void { // onDestruct
+        },
+        [](SocketServer::SocketConnection* socketConnection) -> void { // onConnect
+            VLOG(0) << "\tServer: " + socketConnection->getRemoteAddress().address() + "(" +
+                           socketConnection->getRemoteAddress().address() +
+                           "):" + std::to_string(socketConnection->getRemoteAddress().psm());
+            VLOG(0) << "\tClient: " + socketConnection->getLocalAddress().address() + "(" + socketConnection->getLocalAddress().address() +
+                           "):" + std::to_string(socketConnection->getLocalAddress().psm());
+        },
+        [](SocketServer::SocketConnection* socketConnection) -> void { // onDisconnect
+            VLOG(0) << "OnDisconnect";
+            VLOG(0) << "\tServer: " + socketConnection->getRemoteAddress().address() + "(" +
+                           socketConnection->getRemoteAddress().address() +
+                           "):" + std::to_string(socketConnection->getRemoteAddress().psm());
+            VLOG(0) << "\tClient: " + socketConnection->getLocalAddress().address() + "(" + socketConnection->getLocalAddress().address() +
+                           "):" + std::to_string(socketConnection->getLocalAddress().psm());
+        },
+        [](SocketServer::SocketConnection* socketConnection, const char* junk, ssize_t junkLen) -> void { // onRead
+            std::string data(junk, junkLen);
+            VLOG(0) << "Data to reflect: " << data;
+            socketConnection->enqueue(data);
+        },
+        []([[maybe_unused]] SocketServer::SocketConnection* socketConnection, [[maybe_unused]] int errnum) -> void { // onReadError
+            VLOG(0) << "OnReadError: " << errnum;
+        },
+        []([[maybe_unused]] SocketServer::SocketConnection* socketConnection, [[maybe_unused]] int errnum) -> void { // onWriteError
+            VLOG(0) << "OnWriteError: " << errnum;
+        });
+
+    btServer.listen("5C:C5:D4:B8:3C:AA", 0x1021, 5, [](int errnum) -> void { // calisto
+        if (errnum != 0) {
+            LOG(ERROR) << "BT listen: " << errnum;
+        } else {
+            LOG(INFO) << "BT listening on channel 1";
+        }
+    });
+
+    return net::EventLoop::start();
+}
