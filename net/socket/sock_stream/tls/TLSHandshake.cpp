f645848b45a756158d48b682229f5fe9cc05e6c8
Volker Christian <volker.christian@fh-hagenberg.at>
2020-12-02 20:49:21 +0100

    Polish

--- net/socket/sock_stream/tls/TLSHandshake.cpp
+++ net/socket/sock_stream/tls/TLSHandshake.cpp
@@ -15,6 +15,7 @@
  * You should have received a copy of the GNU Lesser General Public License
  * along with this program. If not, see <http://www.gnu.org/licenses/>.
  */
+
 #ifndef DOXYGEN_SHOULD_SKIP_THIS
 
 #include <openssl/ssl.h> // IWYU pragma: keep
