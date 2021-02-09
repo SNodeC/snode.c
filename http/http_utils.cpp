6a436d16dd4d3d761ca173a4dcb614e9393ca536
Volker Christian <volker.christian@fh-hagenberg.at>
2021-02-09 12:40:02 +0100

    Polish

--- http/http_utils.cpp
+++ http/http_utils.cpp
@@ -68,11 +68,9 @@ namespace httputils {
         for (std::string::const_iterator i = text.begin(), n = text.end(); i != n; ++i) {
             std::string::value_type c = (*i);
 
-            // Keep alphanumeric and other accepted characters intact
             if (isalnum(c) || c == '-' || c == '_' || c == '.' || c == '~') {
                 escaped << c;
             } else {
-                // Any other characters are percent-encoded
                 escaped << std::uppercase;
                 escaped << '%' << std::setw(2) << int((unsigned char) c);
                 escaped << std::nouppercase;
