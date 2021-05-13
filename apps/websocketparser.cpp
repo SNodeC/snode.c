f8f40d7eacf30fba75674e3480d7ff78dc5993c7
Volker Christian <volker.christian@fh-hagenberg.at>
2021-05-13 11:09:25 +0200

    Add test frames

--- apps/websocketparser.cpp
+++ apps/websocketparser.cpp
@@ -6,11 +6,33 @@
 #include <endian.h>
 #include <iomanip>
 #include <iostream>
+#include <openssl/evp.h>
+#include <openssl/sha.h>
 #include <vector>
 
 using namespace net;
 
 using namespace net::socket::ip::tcp::ipv4::legacy;
+
+char* base64(const unsigned char* input, int length) {
+    const int pl = 4 * ((length + 2) / 3);
+    char* output = reinterpret_cast<char*>(calloc(pl + 1, 1)); //+1 for the terminating null that EVP_EncodeBlock adds on
+    const int ol = EVP_EncodeBlock(reinterpret_cast<unsigned char*>(output), input, length);
+    if (pl != ol) {
+        std::cerr << "Whoops, encode predicted " << pl << " but we got " << ol << "\n";
+    }
+    return output;
+}
+
+unsigned char* decode64(const char* input, int length) {
+    const int pl = 3 * length / 4;
+    auto output = reinterpret_cast<unsigned char*>(calloc(pl + 1, 1));
+    const int ol = EVP_DecodeBlock(output, reinterpret_cast<const unsigned char*>(input), length);
+    if (pl != ol) {
+        std::cerr << "Whoops, decode predicted " << pl << " but we got " << ol << "\n";
+    }
+    return output;
+}
 
 union MaskingKeyAsArray {
     uint32_t value;
@@ -25,6 +47,44 @@ public:
     }
     std::vector<char> message;
     unsigned char opCode = 0;
+
+    //   |Opcode  | Meaning                             | Reference |
+    //  -+--------+-------------------------------------+-----------|
+    //   | 0      | Continuation Frame                  | RFC XXXX  |
+    //  -+--------+-------------------------------------+-----------|
+    //   | 1      | Text Frame                          | RFC XXXX  |
+    //  -+--------+-------------------------------------+-----------|
+    //   | 2      | Binary Frame                        | RFC XXXX  |
+    //  -+--------+-------------------------------------+-----------|
+    //   | 8      | Connection Close Frame              | RFC XXXX  |
+    //  -+--------+-------------------------------------+-----------|
+    //   | 9      | Ping Frame                          | RFC XXXX  |
+    //  -+--------+-------------------------------------+-----------|
+    //   | 10     | Pong Frame                          | RFC XXXX  |
+    //  -+--------+-------------------------------------+-----------|
+
+    //   | Version Number | Reference                               |
+    //  -+----------------+-----------------------------------------+-
+    //   | 0              + draft-ietf-hybi-thewebsocketprotocol-00 |
+    //  -+----------------+-----------------------------------------+-
+    //   | 1              + draft-ietf-hybi-thewebsocketprotocol-01 |
+    //  -+----------------+-----------------------------------------+-
+    //   | 2              + draft-ietf-hybi-thewebsocketprotocol-02 |
+    //  -+----------------+-----------------------------------------+-
+    //   | 3              + draft-ietf-hybi-thewebsocketprotocol-03 |
+    //  -+----------------+-----------------------------------------+-
+    //   | 4              + draft-ietf-hybi-thewebsocketprotocol-04 |
+    //  -+----------------+-----------------------------------------+-
+    //   | 5              + draft-ietf-hybi-thewebsocketprotocol-05 |
+    //  -+----------------+-----------------------------------------+-
+    //   | 6              + draft-ietf-hybi-thewebsocketprotocol-06 |
+    //  -+----------------+-----------------------------------------+-
+    //   | 7              + draft-ietf-hybi-thewebsocketprotocol-07 |
+    //  -+----------------+-----------------------------------------+-
+    //   | 8              + draft-ietf-hybi-thewebsocketprotocol-08 |
+    //  -+----------------+-----------------------------------------+-
+    //   | 9              + draft-ietf-hybi-thewebsocketprotocol-09 |
+    //  -+----------------+-----------------------------------------+-
 };
 
 class WebSocketReceiver {
@@ -71,7 +131,7 @@ protected:
         if (junkLen > 0) {
             consumed++;
 
-            uint8_t opCodeByte = static_cast<uint8_t>(junk[0]);
+            uint8_t opCodeByte = *reinterpret_cast<uint8_t*>(junk);
 
             fin = opCodeByte & 0b10000000;
             opCode = (opCodeByte & 0b00001111) == 0 ? opCode : opCodeByte & 0b00001111;
@@ -86,7 +146,7 @@ protected:
         uint64_t consumed = 0;
 
         if (junkLen > 0) {
-            uint8_t lengthByte = static_cast<uint8_t>(junk[0]);
+            uint8_t lengthByte = *reinterpret_cast<uint8_t*>(junk);
 
             masked = lengthByte & 0b10000000;
             length = lengthByte & 0b01111111;
@@ -117,7 +177,8 @@ protected:
         elengthNumBytesLeft = (elengthNumBytesLeft == 0) ? elengthNumBytes : elengthNumBytesLeft;
 
         while (consumed < junkLen && elengthNumBytesLeft > 0) {
-            length |= static_cast<uint64_t>(junk[consumed]) << (elengthNumBytes - elengthNumBytesLeft) * 8;
+            length |= static_cast<uint64_t>(*reinterpret_cast<unsigned char*>(junk + consumed))
+                      << (elengthNumBytes - elengthNumBytesLeft) * 8;
 
             consumed++;
             elengthNumBytesLeft--;
@@ -146,7 +207,8 @@ protected:
             maskingKeyNumBytesLeft = (maskingKeyNumBytesLeft == 0) ? maskingKeyNumBytes : maskingKeyNumBytesLeft;
 
             while (consumed < junkLen && maskingKeyNumBytesLeft > 0) {
-                maskingKey |= static_cast<uint32_t>(junk[consumed]) << (maskingKeyNumBytes - maskingKeyNumBytesLeft) * 8;
+                maskingKey |= static_cast<uint32_t>(*reinterpret_cast<unsigned char*>(junk + consumed))
+                              << (maskingKeyNumBytes - maskingKeyNumBytesLeft) * 8;
                 consumed++;
                 maskingKeyNumBytesLeft--;
             }
@@ -286,8 +348,8 @@ protected:
         char* frame = nullptr;
         uint64_t length = 0;
 
-        if (payloadLength > 125) {
-            if (payloadLength > 0xFFFF) {
+        if (payloadLength > 125 || true) {
+            if (payloadLength > 0xFFFF || true) {
                 frameLength += 8;
                 maskingKeyOffset += 8;
                 payloadOffset += 8;
@@ -354,17 +416,68 @@ protected:
     uint8_t payloadOffset = 2;
 };
 
+std::string serverWebSocketKey(const std::string& clientWebSocketKey) {
+    std::string GUID = "258EAFA5-E914-47DA-95CA-C5AB0DC85B11";
+
+    std::string serverWebSocketKey(clientWebSocketKey + GUID);
+    unsigned char digest[SHA_DIGEST_LENGTH];
+    SHA1(reinterpret_cast<const unsigned char*>(serverWebSocketKey.c_str()),
+         serverWebSocketKey.length(),
+         reinterpret_cast<unsigned char*>(&digest));
+
+    return base64(digest, SHA_DIGEST_LENGTH);
+}
+
 int main(int argc, char* argv[]) {
     SNodeC::init(argc, argv);
 
     WebSocketSender webSocketSender;
 
-    const char* message = "Hallo Du, heute ist ein schöner Tag oder meinst du nicht?"
-                          "Hallo Du, heute ist ein schöner Tag oder meinst du nicht?"
-                          "Hallo Du, heute ist ein schöner Tag oder meinst du nicht?"
-                          "Hallo Du, heute ist ein schöner Tag oder meinst du nicht?";
+    [[maybe_unused]] const char* message = "Hallo Du, heute ist ein schöner Tag oder meinst du nicht?"
+                                           "Hallo Du, heute ist ein schöner Tag oder meinst du nicht?"
+                                           "Hallo Du, heute ist ein schöner Tag oder meinst du nicht?"
+                                           "Hallo Du, heute ist ein schöner Tag oder meinst du nicht?";
 
     webSocketSender.send(1, message, std::string(message).length());
+
+    std::string clientWebSocketKey = "dGhlIHNhbXBsZSBub25jZQ==";
+
+    std::cout << std::dec << "Base64: " << serverWebSocketKey(clientWebSocketKey) << std::endl;
+    std::cout << "Should: "
+              << "s3pPLMBiTxaQ9kYGzzhZRbK+xOo=" << std::endl;
+
+    //    This section is non-normative.
+    //    A single-frame unmasked text message
+    //    0x81 0x05 0x48 0x65 0x6c 0x6c 0x6f (contains "Hello")
+    //    A single-frame masked text message
+    //    0x81 0x85 0x37 0xfa 0x21 0x3d 0x7f 0x9f 0x4d 0x51 0x58 (contains "Hello")
+    //    A fragmented unmasked text message
+    //    0x01 0x03 0x48 0x65 0x6c (contains "Hel")
+    //    0x80 0x02 0x6c 0x6f (contains "lo")
+    //    Ping request and response
+    //    0x89 0x05 0x48 0x65 0x6c 0x6c 0x6f (contains a body of "Hello", but the contents of the body are arbitrary)
+    //    0x8a 0x05 0x48 0x65 0x6c 0x6c 0x6f (contains a body of "Hello", matching the body of the ping)
+    //    256 bytes binary message in a single unmasked frame
+    //    0x82 0x7E 0x0100 [256 bytes of binary data]
+    //    64KiB binary message in a single unmasked frame
+    //    0x82 0x7F 0x0000000000010000 [65536 bytes of binary data]
+
+    WebSocketReceiver webSocketReceiver;
+
+    std::string s("\x81\x05\x48\x65\x6c\x6c\x6f");
+    webSocketReceiver.receive(s.data(), s.length());
+
+    s = "\x81\x85\x37\xfa\x21\x3d\x7f\x9f\x4d\x51\x58";
+    webSocketReceiver.receive(s.data(), s.length());
+
+    s = "\x01\x03\x48\x65\x6c";
+    webSocketReceiver.receive(s.data(), s.length());
+    s = "\x80\x02\x6c\x6f";
+    webSocketReceiver.receive(s.data(), s.length());
+
+    std::cout << "====================================" << std::endl;
+
+    //    frame = {};
 
     SocketServer webSocketParser(
         [](const SocketServer::SocketAddress& localAddress,
