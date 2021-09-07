93c5158d070b2fab79773aa7d02991c793639c0e
Volker Christian <volker.christian@fh-hagenberg.at>
2021-09-07 15:33:58 +0200

    Fix naming

--- express/Response.cpp
+++ express/Response.cpp
@@ -52,7 +52,7 @@ namespace express {
                 headers.insert_or_assign("Content-Length", std::to_string(std::filesystem::file_size(absolutFileName)));
 
                 FileReader::connect(absolutFileName, *this, [this, onError](int err) -> void {
-                    serverContext->terminateConnection();
+                    socketContext->terminateConnection();
                     onError(err);
                 });
             } else {
