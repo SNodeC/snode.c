97d0e5821dcbfe14837c146bdcc8df44528f81aa
Volker Christian <volker.christian@fh-hagenberg.at>
2021-09-30 13:34:18 +0200

    cmake-format: *.cmake.in are formated also

--- cmake/cmake-format.cmake
+++ cmake/cmake-format.cmake
@@ -4,7 +4,7 @@ find_program(CMAKE_FORMAT "cmake-format")
 
 if(CMAKE_FORMAT)
     file(GLOB_RECURSE CMAKELISTS_TXT_FILES ${CMAKE_SOURCE_DIR}/CMakeLists.txt
-         ${CMAKE_SOURCE_DIR}/cmake/*.cmake
+         ${CMAKE_SOURCE_DIR}/cmake/*.cmake ${CMAKE_SOURCE_DIR}/*.cmake.in
     )
     add_custom_command(
         OUTPUT format-cmds
