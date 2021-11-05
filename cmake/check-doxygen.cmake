23d470f02f936194d76dbacc31356e763c23ca73
Volker Christian <volker.christian@fh-hagenberg.at>
2021-11-05 20:58:43 +0100

    Fix rm -f path name

--- cmake/check-doxygen.cmake
+++ cmake/check-doxygen.cmake
@@ -14,7 +14,7 @@ if(DOXYGEN_FOUND)
         add_custom_target(
             doc
             COMMAND rm -rf ${CMAKE_SOURCE_DIR}/../snode.c-doc/html
-            COMMAND rm -f ${CMAKE_SOURCE_DIR}/inline_umlgraph_cache_all.pu
+            COMMAND rm -f ${CMAKE_SOURCE_DIR}/../snode.c-doc/inline_umlgraph_cache_all.pu
             COMMAND ${DOXYGEN_EXECUTABLE} ${DOXYFILE}
             WORKING_DIRECTORY ${CMAKE_SOURCE_DIR}
             COMMENT "Generating API documentation with Doxygen"
@@ -23,7 +23,9 @@ if(DOXYGEN_FOUND)
 
         add_custom_target(
             doc-fast
-            COMMAND rm -f ${CMAKE_SOURCE_DIR}/../snode.c-doc/inline_umlgraph_cache_all.pu
+            COMMAND
+                rm -f
+                ${CMAKE_SOURCE_DIR}/../snode.c-doc/inline_umlgraph_cache_all.pu
             COMMAND ${DOXYGEN_EXECUTABLE} ${DOXYFILE}
             WORKING_DIRECTORY ${CMAKE_SOURCE_DIR}
             COMMENT "Generating API documentation with Doxygen"
