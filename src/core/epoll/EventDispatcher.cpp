8d91aa7adb481a7c07588ccd1a452ba6da9cf990
Volker Christian <volker.christian@fh-hagenberg.at>
2022-01-11 15:30:44 +0100

    Call dispatchActiveEvents only in case events are pending

--- src/core/epoll/EventDispatcher.cpp
+++ src/core/epoll/EventDispatcher.cpp
@@ -83,7 +83,9 @@ namespace core::epoll {
     void EventDispatcher::dispatchActiveEvents(int count, const utils::Timeval& currentTime) {
         if (count > 0) {
             for (int i = 0; i < count; i++) {
-                static_cast<DescriptorEventDispatcher*>(ePollEvents[i].data.ptr)->dispatchActiveEvents(currentTime);
+                if ((ePollEvents[i].events & EPOLLIN) != 0) {
+                    static_cast<DescriptorEventDispatcher*>(ePollEvents[i].data.ptr)->dispatchActiveEvents(currentTime);
+                }
             }
         }
 
