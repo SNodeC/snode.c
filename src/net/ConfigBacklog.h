d3e57c607d4eba8c27d7b0e6e6e13cfbfd3312d3
Volker Christian <volker.christian@fh-hagenberg.at>
2022-01-30 23:51:21 +0100

    Fix include guards

--- src/net/ConfigBacklog.h
+++ src/net/ConfigBacklog.h
@@ -16,8 +16,8 @@
  * along with this program. If not, see <http://www.gnu.org/licenses/>.
  */
 
-#ifndef NET_SERVERCONFIG_H
-#define NET_SERVERCONFIG_H
+#ifndef NET_CONFIGBACKLOG_H
+#define NET_CONFIGBACKLOG_H
 
 #ifndef DOXYGEN_SHOULD_SKIP_THIS
 
@@ -44,4 +44,4 @@ namespace net {
 
 } // namespace net
 
-#endif // NET_SERVERCONFIG_H
+#endif // NET_CONFIGBACKLOG_H
--- src/net/ConfigConn.h
+++ src/net/ConfigConn.h
@@ -16,8 +16,8 @@
  * along with this program. If not, see <http://www.gnu.org/licenses/>.
  */
 
-#ifndef NET_CONFIG_H
-#define NET_CONFIG_H
+#ifndef NET_CONFIGCONN_H
+#define NET_CONFIGCONN_H
 
 #ifndef DOXYGEN_SHOULD_SKIP_THIS
 
@@ -70,4 +70,4 @@ namespace net {
 
 } // namespace net
 
-#endif // CONFIG_H
+#endif // CONFIGCONN_H
