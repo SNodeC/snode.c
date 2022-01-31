4f277496abd4e51f22085094015791fd4c55305b
Volker Christian <volker.christian@fh-hagenberg.at>
2022-01-31 16:28:27 +0100

    Added ClientConfig.h files in the tls and legacy directories

---
+++ src/net/rf/stream/tls/ClientConfig.h
@@ -0,0 +1,45 @@
+/*
+ * snode.c - a slim toolkit for network communication
+ * Copyright (C) 2020, 2021, 2022 Volker Christian <me@vchrist.at>
+ *
+ * This program is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU Lesser General Public License as published
+ * by the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the
+ * GNU Lesser General Public License for more details.
+ *
+ * You should have received a copy of the GNU Lesser General Public License
+ * along with this program. If not, see <http://www.gnu.org/licenses/>.
+ */
+
+#ifndef NET_RF_STREAM_TLS_CLIENTCONFIG_H
+#define NET_RF_STREAM_TLS_CLIENTCONFIG_H
+
+#include "net/ConfigTls.h"              // IWYU pragma: export
+#include "net/rf/stream/ClientConfig.h" // IWYU pragma: export
+
+#ifndef DOXYGEN_SHOULD_SKIP_THIS
+
+#include <string>
+
+#endif // DOXYGEN_SHOULD_SKIP_THIS
+
+namespace net::rf::stream::tls {
+
+    class ClientConfig
+        : public net::rf::stream::ClientConfig
+        , public net::ConfigTls {
+    public:
+        explicit ClientConfig(const std::string& name)
+            : net::rf::stream::ClientConfig(name)
+            , net::ConfigTls(baseSc) {
+        }
+    };
+
+} // namespace net::rf::stream::tls
+
+#endif // NET_RF_STREAM_TLS_CLIENTCONFIG_H
---
+++ src/net/un/stream/legacy/ClientConfig.h
@@ -0,0 +1,45 @@
+/*
+ * snode.c - a slim toolkit for network communication
+ * Copyright (C) 2020, 2021, 2022 Volker Christian <me@vchrist.at>
+ *
+ * This program is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU Lesser General Public License as published
+ * by the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the
+ * GNU Lesser General Public License for more details.
+ *
+ * You should have received a copy of the GNU Lesser General Public License
+ * along with this program. If not, see <http://www.gnu.org/licenses/>.
+ */
+
+#ifndef NET_UN_STREAM_LEGACY_CLIENTCONFIG_H
+#define NET_UN_STREAM_LEGACY_CLIENTCONFIG_H
+
+#include "net/ConfigLegacy.h"
+#include "net/un/stream/ClientConfig.h"
+
+#ifndef DOXYGEN_SHOULD_SKIP_THIS
+
+#include <string>
+
+#endif // DOXYGEN_SHOULD_SKIP_THIS
+
+namespace net::un::stream::legacy {
+
+    class ClientConfig
+        : public net::un::stream::ClientConfig
+        , public net::ConfigLegacy {
+    public:
+        explicit ClientConfig(const std::string& name)
+            : net::un::stream::ClientConfig(name)
+            , net::ConfigLegacy(baseSc) {
+        }
+    };
+
+} // namespace net::un::stream::legacy
+
+#endif // NET_UN_STREAM_LEGACY_CLIENTCONFIG_H
