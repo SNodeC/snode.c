b3753aeef5060f5f569bfe30189d02720142b908
Rathalin <danielflo1997@hotmail.com>
2022-06-09 21:19:08 +0200

    Add email and password hash to client table

--- src/oauth2/authorization_server/database/insert.sql
+++ src/oauth2/authorization_server/database/insert.sql
@@ -1,6 +1,11 @@
 insert into token values();
 insert into token values();
-insert into client(access_token_id, refresh_token_id) values(
+
+set @password_salt1 = UUID();
+insert into client(email, password_hash, password_salt, access_token_id, refresh_token_id) values(
+    'daniel@flockert.at',
+    SHA2(CONCAT(@password_salt1, 'rathalin'), 256),
+    @password_salt1,
     (select id from token limit 1 offset 0),
     (select id from token limit 1 offset 1)
 );
--- src/oauth2/authorization_server/database/select.sql
+++ src/oauth2/authorization_server/database/select.sql
@@ -1,4 +1,13 @@
-select c.id, c.uuid 'client_id', at.uuid 'access_token', at.expire_date 'access_token_expire_date', rt.uuid 'refresh_token', rt.expire_date 'refresh_token_expire_date'
+select
+    c.id,
+    c.uuid 'client_id',
+    c.email 'email',
+    c.password_hash 'password_hash',
+    c.password_salt 'password_salt',
+    at.uuid 'access_token',
+    at.expire_date 'access_token_expire_date',
+    rt.uuid 'refresh_token',
+    rt.expire_date 'refresh_token_expire_date'
 from client c
 join token at
     on c.access_token_id = at.id
