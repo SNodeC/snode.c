f0fd05534b94976d56c77548fac0c4963fc062f0
Volker Christian <volker.christian@fh-hagenberg.at>
2022-09-18 14:07:37 +0200

    Add qtcreator wizard

---
+++ supplement/qtcreator/templates/wizards/files/SNodeCHeader/file.h
@@ -0,0 +1,59 @@
+/*
+ * snode.c - a slim toolkit for network communication
+ * Copyright (C) 2020, 2021, 2022 Volker Christian <me@vchrist.at>
+ *
+ * This program is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU Lesser General Public License as published
+ * by the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the
+ * GNU Lesser General Public License for more details.
+ *
+ * You should have received a copy of the GNU Lesser General Public License
+ * along with this program. If not, see <http://www.gnu.org/licenses/>.
+ */
+
+@if '%{Cpp:PragmaOnce}'
+#pragma once
+@else
+#ifndef %{GUARD}
+#define %{GUARD}
+@endif
+
+@if '%{Base}'
+%{JS: Cpp.includeStatement('%{Base}', Util.preferredSuffix('text/x-c++hdr'), ['QObject', 'QWidget', 'QMainWindow', 'QQuickItem', 'QSharedData'], '%{TargetPath}')}\
+
+@endif
+#ifndef DOXYGEN_SHOULD_SKIP_THIS
+
+#endif // DOXYGEN_SHOUÖD_SKIP_THIS
+
+@if '%{NameSp}'
+namespace %{NameSp} {
+@endif
+
+@if '%{Base}'
+class %{CN} : public %{Base}
+@else
+class %{CN}
+@endif
+{
+public:
+    %{CN}();
+    %{CN}(const %{CN} &) = default;
+
+    %{CN} &operator=(const %{CN} &) = default;
+
+    ~%{CN}();
+};
+%{JS: Cpp.closeNamespaces('%{Class}')}
+@if '%{NameSp}'
+} // %{NameSp}
+@endif
+
+@if ! '%{Cpp:PragmaOnce}'
+#endif // %{GUARD}
+@endif
