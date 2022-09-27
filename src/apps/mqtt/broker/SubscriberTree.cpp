e856424bf6b1aa7ef81a1c5dcf1b3c47f637a7af
Volker Christian <volker.christian@fh-hagenberg.at>
2022-09-27 13:36:09 +0200

    Polish

--- src/apps/mqtt/broker/SubscriberTree.cpp
+++ src/apps/mqtt/broker/SubscriberTree.cpp
@@ -54,11 +54,8 @@ namespace apps::mqtt::broker {
     }
 
     bool SubscriberTree::unsubscribe(std::string remainingTopicName, apps::mqtt::broker::SocketContext* socketContext) {
-        bool empty = false;
-
         if (remainingTopicName.empty()) {
             subscribers.erase(socketContext);
-            empty = subscribers.empty() && subscribtions.empty();
         } else {
             std::string topicName = remainingTopicName.substr(0, remainingTopicName.find("/"));
             remainingTopicName.erase(0, topicName.size() + 1);
@@ -66,10 +63,9 @@ namespace apps::mqtt::broker {
             if (subscribtions.contains(topicName) && subscribtions.find(topicName)->second.unsubscribe(remainingTopicName, socketContext)) {
                 subscribtions.erase(topicName);
             }
-            empty = subscribers.empty() && subscribtions.empty();
         }
 
-        return empty;
+        return subscribers.empty() && subscribtions.empty();
     }
 
     void SubscriberTree::subscribe(std::string remainingTopicName,
