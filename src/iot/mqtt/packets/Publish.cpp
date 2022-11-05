fbd1ce8124ca7b4ce9e3efb9d3656fd83d68e59b
Volker Christian <volker.christian@fh-hagenberg.at>
2022-11-05 04:37:24 +0100

    Fix position for dup-flag

--- src/iot/mqtt/packets/Publish.cpp
+++ src/iot/mqtt/packets/Publish.cpp
@@ -30,7 +30,7 @@ namespace iot::mqtt::packets {
 
     Publish::Publish(uint16_t packetIdentifier, const std::string& topic, const std::string& message, bool dup, uint8_t qoS, bool retain)
         : iot::mqtt::ControlPacket(MQTT_PUBLISH) {
-        this->flags = static_cast<uint8_t>((dup ? 0x04 : 0x00) | ((qoS << 1) & 0x06) | (retain ? 0x01 : 0x00));
+        this->flags = static_cast<uint8_t>((dup ? 0x08 : 0x00) | ((qoS << 1) & 0x06) | (retain ? 0x01 : 0x00));
         this->packetIdentifier = packetIdentifier;
         this->topic = topic;
         this->message = message;
