4353c5474821ac5aafed647ce93603c73960ce00
Volker Christian <volker.christian@fh-hagenberg.at>
2022-12-08 19:13:22 +0100

    Propagate also onExit() to SubProtocol. Polish

--- src/web/websocket/SubProtocolContext.cpp
+++ src/web/websocket/SubProtocolContext.cpp
@@ -16,7 +16,7 @@
  * along with this program. If not, see <http://www.gnu.org/licenses/>.
  */
 
-#include "SubProtocolContext.h"
+#include "web/websocket/SubProtocolContext.h"
 
 #ifndef DOXYGEN_SHOULD_SKIP_THIS
 
