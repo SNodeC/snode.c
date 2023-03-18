a27f0f97d6a0c4ce5c1eff881743e760ed46dc6a
Volker Christian <me@vchrist.at>
2023-03-18 11:38:28 +0100

    Polish help output (empty lines, "=" for flags)

--- src/utils/Formatter.h
+++ src/utils/Formatter.h
@@ -25,6 +25,7 @@
 
 #include <algorithm>
 #include <iostream>
+#include <set>
 #include <string>
 #include <vector>
 
@@ -206,6 +207,40 @@ namespace CLI {
             return out;
         }
 
+        CLI11_INLINE std::string make_option_opts(const Option* opt) const {
+            std::stringstream out;
+
+            if (!opt->get_option_text().empty()) {
+                out << " " << opt->get_option_text();
+            } else {
+                if (opt->get_type_size() != 0) {
+                    if (!opt->get_type_name().empty())
+                        out << (opt->get_expected_max() == 0 ? "=" : " ") << get_label(opt->get_type_name());
+                    if (!opt->get_default_str().empty())
+                        out << " [" << opt->get_default_str() << "] ";
+                    if (opt->get_expected_max() == detail::expected_max_vector_size)
+                        out << " ...";
+                    else if (opt->get_expected_min() > 1)
+                        out << " x " << opt->get_expected();
+
+                    if (opt->get_required())
+                        out << " " << get_label("REQUIRED");
+                }
+                if (!opt->get_envname().empty())
+                    out << " (" << get_label("Env") << ":" << opt->get_envname() << ")";
+                if (!opt->get_needs().empty()) {
+                    out << " " << get_label("Needs") << ":";
+                    for (const Option* op : opt->get_needs())
+                        out << " " << op->get_name();
+                }
+                if (!opt->get_excludes().empty()) {
+                    out << " " << get_label("Excludes") << ":";
+                    for (const Option* op : opt->get_excludes())
+                        out << " " << op->get_name();
+                }
+            }
+            return out.str();
+        }
         /*
                 CLI11_INLINE std::string make_groups(const App* app, AppFormatMode mode) const {
                     std::string out = Formatter::make_groups(app, mode);
