86dfdafad24a206d07b8cb6bd39dc9074984f502
Volker Christian <me@vchrist.at>
2023-07-30 20:11:41 +0200

    Add graphviz command to plot target dependencies

---
+++ cmake/graphwiz.cmake
@@ -0,0 +1,9 @@
+# Custom target to generate a library dependency graph
+
+add_custom_target(graphviz
+    COMMAND ${CMAKE_COMMAND} "--graphviz=foo.dot" .
+    COMMAND dot -Tpdf foo.dot -o ${CMAKE_SOURCE_DIR}/SNodeC-Lib-Dependencies.pdf
+    COMMAND rm foo.dot.*
+    COMMAND rm foo.dot
+    WORKING_DIRECTORY "${CMAKE_BINARY_DIR}"
+)
