65c50bee66419bc9ec7308c5d2cbc1b156031781
Volker Christian <me@vchrist.at>
2023-09-15 16:31:50 +0200

    Fix: only provide posix signales

--- src/utils/system/signal.cpp
+++ src/utils/system/signal.cpp
@@ -34,12 +34,11 @@ namespace utils::system {
 
     std::string sigabbrev_np(int sig) {
         static std::map<int, std::string> sigMap = {
-            {SIGHUP, "HUP"},    {SIGINT, "INT"},   {SIGQUIT, "QUIT"}, {SIGILL, "ILL"},       {SIGTRAP, "TRAP"},     {SIGABRT, "ABRT"},
-            {SIGIOT, "IOT"},    {SIGBUS, "BUS"},   {SIGFPE, "FPE"},   {SIGKILL, "KILL"},     {SIGUSR1, "USR1"},     {SIGSEGV, "SEGV"},
-            {SIGUSR2, "USR2"},  {SIGPIPE, "PIPE"}, {SIGALRM, "ALRM"}, {SIGTERM, "TERM"},     {SIGSTKFLT, "STKFLT"}, {SIGCHLD, "CHLD"},
-            {SIGCHLD, "CHLD "}, {SIGCONT, "CONT"}, {SIGSTOP, "STOP"}, {SIGTSTP, "TSTP"},     {SIGTTIN, "TTIN"},     {SIGTTOU, "TTOU"},
-            {SIGURG, "URG"},    {SIGXCPU, "XCPU"}, {SIGXFSZ, "XFSZ"}, {SIGVTALRM, "VTALRM"}, {SIGPROF, "PROF"},     {SIGWINCH, "WINCH"},
-            {SIGIO, "IO"},      {SIGPOLL, "POLL"}, {SIGPWR, "PWR"},   {SIGSYS, "SYS"}};
+            {SIGABRT, "ABRT"}, {SIGALRM, "ALRM"},     {SIGBUS, "BUS"},   {SIGCHLD, "CHLD"}, {SIGCONT, "CONT"},  {SIGFPE, "FPE"},
+            {SIGHUP, "HUP"},   {SIGILL, "ILL"},       {SIGINT, "INT"},   {SIGKILL, "KILL"}, {SIGPIPE, "PIPE"},  {SIGPOLL, "POLL"},
+            {SIGPROF, "PROF"}, {SIGQUIT, "QUIT"},     {SIGSEGV, "SEGV"}, {SIGSTOP, "STOP"}, {SIGSYS, "SYS"},    {SIGTERM, "TERM"},
+            {SIGTRAP, "TRAP"}, {SIGTSTP, "TSTP"},     {SIGTTIN, "TTIN"}, {SIGTTOU, "TTOU"}, {SIGUSR1, "USR1"},  {SIGUSR2, "USR2"},
+            {SIGURG, "URG"},   {SIGVTALRM, "VTALRM"}, {SIGXCPU, "XCPU"}, {SIGXFSZ, "XFSZ"}, {SIGWINCH, "WINCH"}};
 
         return sigMap.contains(sig) ? sigMap[sig] : "UNKNOWN";
     }
