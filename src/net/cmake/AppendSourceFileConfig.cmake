10b65a8ac6b741abde4f5717207065d3000a0a47
Volker Christian <me@vchrist.at>
2023-09-28 23:35:24 +0200

    Fix caching of config values

--- src/net/cmake/AppendSourceFileConfig.cmake
+++ src/net/cmake/AppendSourceFileConfig.cmake
@@ -7,15 +7,15 @@ function(append_source_file_config source_file CONFIG_OPTION description default
         set(value ${ARGN})
     endif()
 
-    set(${CONFIG_OPTION}
+    set(CACHE_${CONFIG_OPTION}
         ${value}
         CACHE STRING "${description}"
     )
 
     set_property(
       SOURCE ${source_file}
-      APPEND PROPERTY COMPILE_DEFINITIONS ${CONFIG_OPTION}=${${CONFIG_OPTION}}
+      APPEND PROPERTY COMPILE_DEFINITIONS ${CONFIG_OPTION}=${CACHE_${CONFIG_OPTION}}
     )
 
-    message(STATUS "Appending compile definition to '${source_file}': " '${CONFIG_OPTION}=${${CONFIG_OPTION}}')
+    message(STATUS "Appending compile definition to '${source_file}': " '${CONFIG_OPTION}=${CACHE_${CONFIG_OPTION}}')
 endfunction(append_source_file_config source_file CONFIG_OPTION description default_value)
