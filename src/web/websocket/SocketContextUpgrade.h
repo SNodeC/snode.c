fcfeb2356a9a8c22caad313210ee1daff0323ffc
Volker Christian <me@vchrist.at>
2024-01-14 09:55:46 +0100

    Revert "Auxiliary commit to revert individual files from 22743679c5b92c0cd9321120bd60621bb520b81d"
    This reverts commit 94bbce4ac39463e450e6fff38d97d80cafce75a1.

--- src/web/websocket/SocketContextUpgrade.h
+++ src/web/websocket/SocketContextUpgrade.h
@@ -48,7 +48,7 @@ namespace web::websocket {
 
 #endif /* DOXYGEN_SHOULD_SKIP_THIS */
 
-#define CLOSE_SOCKET_TIMEOUT 1
+#define CLOSE_SOCKET_TIMEOUT 10
 
 namespace web::websocket {
 
@@ -130,13 +130,14 @@ namespace web::websocket {
 
         void sendClose(const char* message, std::size_t messageLength) override {
             if (!closeSent) {
-                LOG(DEBUG) << "WebSocket: Sending close to peeeeeeeeeer";
-
-                closeSent = true;
+                LOG(DEBUG) << "WebSocket: Sending close to peer";
 
                 sendMessage(8, message, messageLength);
+                shutdownWrite();
 
                 setTimeout(CLOSE_SOCKET_TIMEOUT);
+
+                closeSent = true;
             }
         }
 
@@ -193,7 +194,6 @@ namespace web::websocket {
                         LOG(DEBUG) << "WebSocket: Close request received - replying with close";
                         sendClose(pongCloseData.data(), pongCloseData.length());
                         pongCloseData.clear();
-                        shutdownWrite();
                     }
                     break;
                 case SubProtocolContext::OpCode::PING:
