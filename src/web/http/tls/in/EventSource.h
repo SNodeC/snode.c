dbf061403cb9ab9c0492294aa00dca5db494a422
Volker Christian <me@vchrist.at>
2025-10-24 09:53:49 +0200

    Add EventSource to each protocol variant

---
+++ src/web/http/tls/in/EventSource.h
@@ -0,0 +1,81 @@
+/*
+ * SNode.C - A Slim Toolkit for Network Communication
+ * Copyright (C) Volker Christian <me@vchrist.at>
+ *               2020, 2021, 2022, 2023, 2024, 2025
+ *
+ * This program is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU Lesser General Public License as published
+ * by the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the
+ * GNU Lesser General Public License for more details.
+ *
+ * You should have received a copy of the GNU Lesser General Public License
+ * along with this program. If not, see <http://www.gnu.org/licenses/>.
+ */
+
+/*
+ * MIT License
+ *
+ * Permission is hereby granted, free of charge, to any person obtaining a copy
+ * of this software and associated documentation files (the "Software"), to deal
+ * in the Software without restriction, including without limitation the rights
+ * to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+ * copies of the Software, and to permit persons to whom the Software is
+ * furnished to do so, subject to the following conditions:
+ *
+ * The above copyright notice and this permission notice shall be included in
+ * all copies or substantial portions of the Software.
+ *
+ * THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+ * IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+ * FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+ * AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+ * LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+ * OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN
+ * THE SOFTWARE.
+ */
+
+#ifndef WEB_HTTP_TLS_IN_EVENTSOURCE_H
+#define WEB_HTTP_TLS_IN_EVENTSOURCE_H
+
+#include "web/http/client/tools/EventSource.h"
+#include "web/http/tls/in/Client.h"
+
+#include <memory>
+
+#ifndef DOXYGEN_SHOULD_SKIP_THIS
+
+#endif // DOXYGEN_SHOULD_SKIP_THIS
+
+namespace web::http::tls::in {
+
+    class EventSource : public web::http::client::tools::EventSourceT<web::http::tls::in::Client> {
+    private:
+        using Super = web::http::client::tools::EventSourceT<web::http::tls::in::Client>;
+
+        using Super::init;
+        using Super::Super;
+
+    public:
+        inline ~EventSource() override;
+
+        friend inline std::shared_ptr<web::http::client::tools::EventSource> EventSource(const std::string& url);
+    };
+
+    inline EventSource::~EventSource() {
+    }
+
+    inline std::shared_ptr<web::http::client::tools::EventSource> EventSource(const std::string& url) {
+        std::shared_ptr<class EventSource> eventSource = std::make_shared<class EventSource>();
+        eventSource->init(url);
+
+        return eventSource;
+    }
+
+} // namespace web::http::tls::in
+
+#endif // WEB_HTTP_TLS_IN_EVENTSOURCE_H
