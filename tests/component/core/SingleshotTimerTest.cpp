998b1257e120c31595b2ba2ec894e1ec2beeb439
Volker Christian <me@vchrist.at>
2026-07-02 19:48:25 +0200

    Add core timer component tests

---
+++ tests/component/core/SingleshotTimerTest.cpp
@@ -0,0 +1,97 @@
+/*
+ * SNode.C - A Slim Toolkit for Network Communication
+ * Copyright (C) Volker Christian <me@vchrist.at>
+ *               2020, 2021, 2022, 2023, 2024, 2025, 2026
+ *
+ * This program is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU Lesser General Public License as published
+ * by the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the
+ * GNU Lesser General Public License for more details.
+ *
+ * You should have received a copy of the GNU Lesser General Public License
+ * along with this program. If not, see <http://www.gnu.org/licenses/>.
+ */
+
+/*
+ * MIT License
+ *
+ * Permission is hereby granted, free of charge, to any person obtaining a copy
+ * of this software and associated documentation files (the "Software"), to deal
+ * in the Software without restriction, including without limitation the rights
+ * to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+ * copies of the Software, and to permit persons to whom the Software is
+ * furnished to do so, subject to the following conditions:
+ *
+ * The above copyright notice and this permission notice shall be included in
+ * all copies or substantial portions of the Software.
+ *
+ * THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+ * IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+ * FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+ * AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+ * LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+ * OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN
+ * THE SOFTWARE.
+ */
+
+#include "core/SNodeC.h"
+#include "core/timer/Timer.h"
+#include "support/TestResult.h"
+#include "utils/Timeval.h"
+
+#ifndef DOXYGEN_SHOULD_SKIP_THIS
+
+#include <functional>
+#include <grp.h>
+#include <iostream>
+#include <unistd.h>
+
+#endif /* DOXYGEN_SHOULD_SKIP_THIS */
+
+namespace {
+
+    constexpr int cTestSkipReturnCode = 77;
+
+    bool shouldSkipRootWithoutSNodeCGroup() {
+        const bool skip = geteuid() == 0 && getgrnam("snodec") == nullptr;
+
+        return skip;
+    }
+
+} // namespace
+
+int main(int argc, char* argv[]) {
+    tests::support::TestResult testResult;
+    int result = cTestSkipReturnCode;
+
+    if (shouldSkipRootWithoutSNodeCGroup()) {
+        std::cout << "SKIP: SNode.C timer component test is skipped for uid 0 because the configured snodec group is unavailable"
+                  << std::endl;
+    } else {
+        int callbackCount = 0;
+
+        core::SNodeC::init(argc, argv);
+
+        core::timer::Timer singleshotTimer = core::timer::Timer::singleshotTimer(
+            [&callbackCount]() {
+                ++callbackCount;
+                core::SNodeC::stop();
+            },
+            utils::Timeval({0, 1000}));
+
+        const int startResult = core::SNodeC::start(utils::Timeval({1, 0}));
+
+        testResult.expectEqual(1, callbackCount, "single-shot timer fires exactly once");
+        testResult.expectEqual(0, startResult, "event loop stops successfully from the single-shot timer callback");
+
+        core::SNodeC::free();
+        result = testResult.processResult();
+    }
+
+    return result;
+}
